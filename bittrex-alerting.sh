3f850cad0f5a47549dc89a249ade007c396d66a2
Sean Rütschi <bitlog@users.noreply.github.com>
2017-12-15 07:45:30 +0100

    Update script
    - Update bittrex-alerting.sh

--- bittrex-alerting.sh
+++ bittrex-alerting.sh
@@ -2,6 +2,7 @@
 
 
 # set variable variables
+CURRENCIES="BTC ETH"
 CRCYFILE="${HOME}/.currencies"
 SLEEPTIME="10"
 
@@ -16,31 +17,44 @@ TERM_WIDTH="$(tput cols)"
 COLUMNS="$(printf '%*s\n' "${TERM_WIDTH}" '' | tr ' ' -)"
 
 
-if tty -s; then
-  echo
-fi
-
-if [[ ! -f "${CRCYFILE}" ]]; then
-  if tty -s; then
-    echo -e "Need file ${CRCYFILE} to read cryptocurrencies!\n"
-  fi
+# check that script is not running
+RUNS="$(ps faux | grep -v grep | grep ${0} | wc -l)" && ((RUNS--))
+if [[ "${RUNS}" -ne "1" ]]; then
+  echo -e "\n${PRG} is already running. Exiting now!\n"
   exit 1
 fi
 
 
+# prepare output for terminal
+if tty -s; then
+  echo
+fi
+
+
+# check currency file
+if [[ -f "${CRCYFILE}" ]]; then
+  CURRENCIES+=" $(cat ${CRCYFILE})"
+  CURRENCIES="$(echo ${CURRENCIES} | sed -e 's/  //g' -e 's/\t//g' | tr ' ' '\n' | tr '[:upper:]' '[:lower:]' | sort -u | tr '\n' ' ')"
+fi
+
+
+# start alerting
 while true ; do
-  for crc in $(echo "BTC ETH $(cat ${CRCYFILE})" | tr ' ' '\n' | tr '[:upper:]' '[:lower:]' | sort -u | tr '\n' ' '); do
+  for crc in ${CURRENCIES}; do
     CRCY="$(echo "${crc}" | tr '[:lower:]' '[:upper:]')"
     CHCKFILE="/tmp/${PRG}-${crc}"
 
+    # set market
     if [[ "${CRCY}" == "BTC" ]]; then
       MARKET="USDT-${CRCY}"
     else
       MARKET="BTC-${CRCY}"
     fi
 
+    # get online data
     DATA="$(curl -s --connect-timeout 2 -m 5 "https://bittrex.com/api/v1.1/public/getticker?market=${MARKET}" | python -mjson.tool 2> /dev/null)"
 
+    # run through data if obtained
     if echo "${DATA}" | grep -q "\"success\": true"; then
       PRC="$(echo "${DATA}" | grep "\"Bid\"" | awk '{print $2}' | sed -e 's/,$//' -e 's/[eE]+*/\*10\^/' | bc -l | sed -e 's/^\./0./' -e 's/[0]*$//' -e 's/\.$/.00/')"
       if [[ ! -z "${PRC}" ]]; then
@@ -50,14 +64,18 @@ while true ; do
           PRICE="$(echo "${PRC} * 100000000" | bc -l | awk -F'.' '{print $1}')"
         fi
 
+        # set checkfile
         echo "${PRICE}" >> ${CHCKFILE}
 
+        # check checkfile
         if [[ -f ${CHCKFILE} ]]; then
           if [[ "$(wc -l ${CHCKFILE} | awk '{print $1}')" -ge "1" ]]; then
             while read line; do
               if [[ ! -z "${line}" ]]; then
+                # get difference percentage
                 PERC="$(echo "(${line} - ${PRICE}) / ${PRICE} * 100" | bc -l | awk -F'.' '{print $1}' | sed 's/^-//')"
 
+                # set alerting colours for terminal
                 if [[ "$(echo "${PRICE}>${line}" | bc -l)" -ge "1" ]]; then
                   COLOR_ALERT="${COLOR_GREEN}"
                   TEXT_ALERT="more"
@@ -66,7 +84,9 @@ while true ; do
                   TEXT_ALERT="less"
                 fi
 
+                # alert if large change was detected
                 if [[ "${PERC}" -ge "10" ]]; then
+                  # output alert if in terminal
                   if tty -s; then
                     echo -e "${COLUMNS}"
                     OUTPUT="${COLOR_ALERT}${CRCY} > ${PERC}% ${TEXT_ALERT}${COLOR_NONE}\n"
@@ -77,6 +97,7 @@ while true ; do
                     echo -e "${COLUMNS}\n"
                   fi
 
+                  # add to alert file
                   if ! grep -qs "${CRCY}" ${ALRTFILE}; then
                     ALRTFILE_TEXT="${CRCY} $(cat ${ALRTFILE} 2> /dev/null)"
                     echo "${ALRTFILE_TEXT}" | sed 's/[ ]*$//' > ${ALRTFILE}
@@ -87,6 +108,7 @@ while true ; do
             done < ${CHCKFILE}
           fi
 
+          # reduce checkfile
           until [[ "$(wc -l ${CHCKFILE} | awk '{print $1}')" -le "5" ]]; do
             sed -i 1d ${CHCKFILE}
           done
@@ -95,7 +117,8 @@ while true ; do
     fi
   done
 
+  # wait
   sleep ${SLEEPTIME}
 done
 
-exit 0
+exit $?
